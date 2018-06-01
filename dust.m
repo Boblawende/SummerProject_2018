4367b1ed8b69b9fd0bce0b9d106b08f6bc071036
Boblawende <39779335+Boblawende@users.noreply.github.com>
2018-05-31 19:02:09 -0500

    Delete dust.m

--- dust.m
+++
@@ -1,5 +0,0 @@
-ncdisp('http://iridl.ldeo.columbia.edu/home/.nasa_roses_a19/.Dust_model/.dust_mon_avg/.dust_pm25_sconc10_mon/dods')
-Dust=ncread('http://iridl.ldeo.columbia.edu/home/.nasa_roses_a19/.Dust_model/.dust_mon_avg/.dust_pm25_sconc10_mon/dods','dust_pm25_sconc10_mon');
-X=ncread('http://iridl.ldeo.columbia.edu/home/.nasa_roses_a19/.Dust_model/.dust_mon_avg/.dust_pm25_sconc10_mon/dods','X');
-Y=ncread('http://iridl.ldeo.columbia.edu/home/.nasa_roses_a19/.Dust_model/.dust_mon_avg/.dust_pm25_sconc10_mon/dods','Y');
-T=ncread('http://iridl.ldeo.columbia.edu/home/.nasa_roses_a19/.Dust_model/.dust_mon_avg/.dust_pm25_sconc10_mon/dods','T');
